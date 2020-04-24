d1bc012623eaea0d4c35b574a55ba3910ba91e1a
Hans Goedegebuure <hansgoed@gmail.com>
2020-04-24 15:11:46 +0200

    [feat] Added endpoints for events filtered by road.

--- src/Controller/RoadController.php
+++ src/Controller/RoadController.php
@@ -73,4 +73,50 @@ class RoadController
 
         return new JsonResponse($normalizedRoad);
     }
+
+    /**
+     * @Route("/roads/{name}/traffic_jams")
+     */
+    public function showTrafficJams(Road $road)
+    {
+        $normalizedTrafficJams = $this->normalizeEvents($road->getTrafficJams());
+
+        return new JsonResponse($normalizedTrafficJams);
+    }
+
+    /**
+     * @Route("/roads/{name}/roadworks")
+     */
+    public function showRoadworks(Road $road)
+    {
+        $normalizedRoadworks = $this->normalizeEvents($road->getRoadworks());
+
+        return new JsonResponse($normalizedRoadworks);
+    }
+
+    /**
+     * @Route("/roads/{name}/radars")
+     */
+    public function showRadars(Road $road)
+    {
+        $normalizedRadars = $this->normalizeEvents($road->getRadars());
+
+        return new JsonResponse($normalizedRadars);
+    }
+
+    /**
+     * @param EventInterface[]|Collection $events
+     */
+    private function normalizeEvents(Collection $events)
+    {
+        return $this->serializer->normalize(
+            $events,
+            null,
+            [
+                AbstractObjectNormalizer::GROUPS => [
+                    self::SERIALIZATION_CONTEXT_EVENT,
+                ],
+            ]
+        );
+    }
 }
