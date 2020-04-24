971ce672d995bc186d5305bd878f695e0be41e82
Hans Goedegebuure <hansgoed@gmail.com>
2020-04-24 15:58:18 +0200

    [fix] Fixed double declared constructor for all repositories that exted the AbstractEventRepository

--- src/Repository/AbstractEventRepository.php
+++ src/Repository/AbstractEventRepository.php
@@ -9,11 +9,6 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 abstract class AbstractEventRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, AbstractEvent::class);
-    }
-
     /**
      * @return AbstractEvent[]
      */
