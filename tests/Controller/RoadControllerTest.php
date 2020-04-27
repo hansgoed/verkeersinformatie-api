4fa243727ba784029dabd08376b21f5ffe1293d4
hans <hansgoed@gmail.com>
2020-04-27 20:11:00 +0200

    [test] Added test for RoadController::showTrafficJams

--- tests/Controller/RoadControllerTest.php
+++ tests/Controller/RoadControllerTest.php
@@ -3,7 +3,10 @@
 namespace App\Tests\Controller;
 
 use App\Controller\RoadController;
+use App\Entity\Event\TrafficJam;
+use App\Entity\Location;
 use App\Entity\Road;
+use Doctrine\Common\Collections\ArrayCollection;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -61,6 +64,55 @@ class RoadControllerTest extends TestCase
         );
 
         $response = $this->roadController->show($road, $request);
+
+        $this->assertEquals($expectedResponse, $response);
+    }
+
+    /**
+     * Test that {@see RoadController::showTrafficJams} fetches traffic jams from road object, serializes them and
+     * returns them.
+     *
+     * @dataProvider validQueryParametersProvider
+     */
+    public function testShowTrafficJams(array $queryParameters) {
+        /** @var Road|MockObject $roadMock */
+        $roadMock = $this->getMockBuilder(Road::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $trafficJams = new ArrayCollection([
+            'a',
+            'b',
+        ]);
+
+        $roadMock->expects($this->once())
+            ->method('getTrafficJams')
+            ->willReturn($trafficJams);
+
+        $normalizedTrafficJams = ['hi' => 'there'];
+        $this->serializerMock->expects($this->once())
+            ->method('normalize')
+            ->with(
+                $trafficJams,
+                null,
+                [
+                    'groups' => [
+                        'event_context',
+                    ],
+                ]
+            )
+            ->willReturn($normalizedTrafficJams);
+
+        $request = new Request($queryParameters);
+        $response = $this->roadController->showTrafficJams($roadMock, $request);
+
+        $expectedResponse = new JsonResponse(
+            $normalizedTrafficJams,
+            200,
+            [
+                'Access-Control-Allow-Origin' => '*'
+            ]
+        );
 
         $this->assertEquals($expectedResponse, $response);
     }
